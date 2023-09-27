afb54a388e3fbff44090fbcd0f4b07ed4d729360
Talita-S <talitassantos2015@gmail.com>
2023-09-27 09:21:40 -0300

    feat: List followers and Unfollow User implemented

--- src/main/java/io/github/talita/s/quarkussocial/rest/FollowerResource.java
+++ src/main/java/io/github/talita/s/quarkussocial/rest/FollowerResource.java
@@ -4,11 +4,15 @@ import io.github.talita.s.quarkussocial.domain.model.Follower;
 import io.github.talita.s.quarkussocial.domain.repository.FollowerRepository;
 import io.github.talita.s.quarkussocial.domain.repository.UserRepository;
 import io.github.talita.s.quarkussocial.rest.dto.FollowerRequest;
+import io.github.talita.s.quarkussocial.rest.dto.FollowerResponse;
+import io.github.talita.s.quarkussocial.rest.dto.FollowersPerUserResponse;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.stream.Collectors;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -28,6 +32,12 @@ public class FollowerResource {
     @Transactional
     public Response followUser(
             @PathParam("userId") Long userId, FollowerRequest request){
+
+        if(userId.equals(request.getFollowerId())){
+            return Response.status(Response.Status.CONFLICT)
+                    .entity("You can't follow yourself")
+                    .build();
+        }
 
         var user = userRepository.findById(userId);
 
@@ -49,6 +59,40 @@ public class FollowerResource {
 
         return  Response.status(Response.Status.NO_CONTENT).build();
 
+    }
+
+    @GET
+    public Response listFollowers(@PathParam("userId") Long userId){
+        var user = userRepository.findById(userId);
+
+        if(user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        var list = repository.findByUser(userId);
+        FollowersPerUserResponse responseObject = new FollowersPerUserResponse();
+        responseObject.setFollowersCount(list.size());
+
+        var followersList = list.stream()
+                .map(FollowerResponse::new)
+                .collect(Collectors.toList());
+
+        responseObject.setContent(followersList);
+        return Response.ok(responseObject).build();
+    }
+
+    @DELETE
+    @Transactional
+    public Response unfollowUser(@PathParam("userId") Long userId, @QueryParam("followerId") Long followerId){
+        var user = userRepository.findById(userId);
+
+        if(user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        repository.deleteByFollowerAndUser(followerId, userId);
+
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 
 }
